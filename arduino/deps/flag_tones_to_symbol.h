2fbf01bf826cc3b0c38a9aab70d9daff61292281
Myrns <laviec.mael@outlook.fr>
2023-11-12 18:29:46 +0100

    chore : added flagÃ_tones_to_symbol.cpp

---
+++ arduino/deps/flag_tones_to_symbol.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef FLAG_TONES_TO_SYMBOL_H_INCLUDED
+#define FLAG_TONES_TO_SYMBOL_H_INCLUDED
+
+int flag_tones_to_symbol(float* flag);
+
+#endif /*FLAG_TONES_TO_SYMBOL_H_INCLUDED*/
